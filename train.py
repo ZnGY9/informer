fdb935a9da90bd458becaab6250c82dd2e31be72
ZnGY9 <2449119602@qq.com>
2024-05-28 22:34:20 +0800

    [informer]

---
+++ train.py
@@ -0,0 +1,12 @@
+import torch
+from torch import nn
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+import pandas as pd
+class MyClass:
+    def __init__(self):
+    def my_method(self, x):
+        y = x + 2
+       print（y）
+class1 = MyClass()
+class1(1)
